cad8962267ce8f72f1b00e6918380bb8c216e0fb
shaked <shakedlevi32@gmail.com>
2022-11-03 19:23:07 +0200

    Login screen code

--- app/src/main/java/com/example/ev_sc/LoginScreen.java
+++ app/src/main/java/com/example/ev_sc/LoginScreen.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginScreen extends Activity{
+public class LoginScreen extends Activity {
 
     Button login_button;
     Button register_button_login;
@@ -23,20 +23,22 @@ public class LoginScreen extends Activity{
     TextView password_view_login;
 
     @Override
-    protected void onCreate(Bundle Instance)
-    {
+    protected void onCreate(Bundle Instance) {
         super.onCreate(Instance);
         setContentView(R.layout.login);
 
-        login_button = (Button)findViewById(R.id.login_button);
-        register_button_login = (Button)findViewById(R.id.register_button_login);
+        login_button = (Button) findViewById(R.id.login_button);
+        register_button_login = (Button) findViewById(R.id.register_button_login);
 
-        username_enter_login =(EditText)(findViewById(R.id.username_enter_login));
-        password_enter_login =(EditText)(findViewById(R.id.password_enter_login));
+        username_enter_login = (EditText) (findViewById(R.id.username_enter_login));
+        password_enter_login = (EditText) (findViewById(R.id.password_enter_login));
+
+        title_login = (TextView) (findViewById(R.id.title_login));
+        username_view_login = (TextView) (findViewById(R.id.username_view_login));
+        password_view_login = (TextView) (findViewById(R.id.password_view_login));
 
 
     }
-
 
 
 }
