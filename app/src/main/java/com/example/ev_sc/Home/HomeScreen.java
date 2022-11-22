a0695a390668a69aec1e18ed23a81cad5ce55ba5
shaked <shakedlevi32@gmail.com>
2022-11-22 14:21:47 +0200

    Added location services to map

--- app/src/main/java/com/example/ev_sc/Home/HomeScreen.java
+++ app/src/main/java/com/example/ev_sc/Home/HomeScreen.java
@@ -1,60 +1,175 @@
 package com.example.ev_sc.Home;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-
-import android.widget.Button;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import com.example.ev_sc.Login.LoginScreen;
 import com.example.ev_sc.R;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class HomeScreen extends FragmentActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+public class HomeScreen extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MapActivity";
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    /*vars*/
+    private Boolean mLocationPermissionGranted = false;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
 
     @Override
-    public void onCreate(Bundle Instance)
-    {
+    public void onCreate(Bundle Instance) {
         super.onCreate(Instance);
         setContentView(R.layout.home);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_home_screen);
-
-        assert mapFragment != null;
-        mapFragment.getMapAsync(this);
+        getLocationPermission();
     }
 
+    /**
+     * this method simply gets the device location, in order to pinpoint it on the map for the user.
+     */
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting the device current location.");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (mLocationPermissionGranted) {
+                Task<Location> location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "onComplete getDeviceLocation: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            //move camera to the current location of the user//
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                        } else {
+                            Log.d(TAG, "onComplete getDeviceLocation: current location is null");
+                            Toast.makeText(HomeScreen.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: Security Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * this method moves the current camera to a new location on the map
+     *
+     * @param latLng coordinates of the location (latitude,longitude)
+     * @param zoom   float representing the desired zooming of the map.
+     */
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: Moving the camera to: (lat: " + latLng.latitude + ", lng: " + latLng.longitude + " )");
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    /**
+     * method to set the map.
+     *
+     * @param googleMap google map object
+     */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
+        Log.d(TAG, "Map is ready");
 
-        LatLng Beersheva = new LatLng(31.284275, 34.873075);
-        map.addMarker(new MarkerOptions().position(Beersheva).title("Beersheva"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Beersheva));
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+        }
     }
-}
 
-//        logout_button = (Button) findViewById(R.id.logout_button_home_screen);
-//
-//        logout_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                FirebaseAuth.getInstance().signOut(); // logout user
-//                startActivity(new Intent(getApplicationContext(), LoginScreen.class));
-//                finish();
-//            }
-//        });
+
+    private void initMap() {
+        Log.d(TAG, "initMap: initializing map...");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_home_screen);
+        mapFragment.getMapAsync(HomeScreen.this);
+    }
+
+    /**
+     * This method gets the permissions for the location of the user.
+     */
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location...");
+
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initMap();
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * this method supposed to check permission for loading the map.
+     */
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionResult: Called.");
+
+        mLocationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    // check case if all permissions are granted //
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionResult: Permission failed..");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionResult: Permission granted..");
+
+                    mLocationPermissionGranted = true;
+                    //init map//
+                    initMap();
+                }
+            }
+        }
+    }
+
+
+}
