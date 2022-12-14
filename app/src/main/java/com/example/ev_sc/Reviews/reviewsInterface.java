8ec1e82925ba167842d561463e8ece5e55880122
liornagar <liornagar799gmail.com>
2022-12-14 12:34:10 +0200

    new uml

---
+++ app/src/main/java/com/example/ev_sc/Reviews/reviewsInterface.java
@@ -0,0 +1,23 @@
+package com.example.ev_sc.Reviews;
+
+public interface reviewsInterface {
+    /**
+     * @return String representing the UID of the user
+     */
+    public String getUID_user();
+    /**
+     * @return Double representing the Stars of the Review
+     */
+    public Double getStars();
+    /**
+     * @return String representing the Review
+     */
+    public String getReview();
+
+
+    public void setUID_user(String uid);
+
+    public void setStars(Double x);
+
+    public void setReview(String s);
+}
