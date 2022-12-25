82f07a17a28f4bed49229bcf632a259e35aa3419
shaked <shakedlevi32@gmail.com>
2022-12-25 13:02:51 +0200

    Favorite Obj creation with database parser

--- app/src/main/java/com/example/ev_sc/Profile/Favorites/FavoritesDB.java
+++ app/src/main/java/com/example/ev_sc/Profile/Favorites/FavoritesDB.java
@@ -1,4 +1,52 @@
 package com.example.ev_sc.Profile.Favorites;
 
+import android.util.Log;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FavoritesDB {
+
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+
+    final private String TAG = "FavoritesDB";
+
+    /**
+     * this method adds a new favorite station to the database.
+     */
+    public void AddFavoriteStationToDB(FavoriteObj curr_fav, String UID) {
+        Log.d(TAG, "Added new favorite Station: => " + curr_fav.toString() + " For user: => " + UID);
+        CollectionReference f = fStore.collection("users").document(UID)
+                .collection("favorites");
+
+        Map<String, Object> fav_station = new HashMap<>();
+        fav_station.put("Station", curr_fav.getStation_name());
+        fav_station.put("SID", curr_fav.getStation_id());
+        fav_station.put("Location", curr_fav.getStation_GeoPoint());
+
+        //Check//
+        f.add(fav_station).addOnSuccessListener(unused -> Log.d(TAG, "reviews Profile is created for "));
+
+    }
+
+    /**
+     * this method parses a firebase Favorite document into a Favorite object.
+     *
+     * @param doc Firebase Favorite Document
+     * @return Object Of Favorite Type.
+     */
+    public FavoriteObj GetFavoriteStationFromDB(QueryDocumentSnapshot doc) {
+        //parser from firebase to object
+        String s_name = doc.getString("Station");
+        String SID = doc.getString("SID");
+        GeoPoint s_loc = doc.getGeoPoint("Location");
+
+        return new FavoriteObj(SID, s_name, s_loc);
+
+    }
 }
--- app/src/main/java/com/example/ev_sc/Reviews/reviewsObj.java
+++ app/src/main/java/com/example/ev_sc/Reviews/reviewsObj.java
@@ -2,44 +2,48 @@ package com.example.ev_sc.Reviews;
 
 public class reviewsObj {
 
-   private String UID_user;
-   private Double stars;
-   private String review;
+    private String UID_user;
+    private Double stars;
+    private String review;
 
 
-   public reviewsObj(String UID_user , Double x , String review){
+    public reviewsObj(String UID_user, Double x, String review) {
 
-       this.UID_user = UID_user;
-       this.stars=x;
-       this.review=review;
-   }
+        this.UID_user = UID_user;
+        this.stars = x;
+        this.review = review;
+    }
 
-    public reviewsObj(reviewsObj review){
+    public reviewsObj(reviewsObj review) {
 
         this.UID_user = review.UID_user;
-        this.stars= review.stars;
-        this.review= review.review;
+        this.stars = review.stars;
+        this.review = review.review;
     }
 
 
-    public String getUID_user(){
-     return this.UID_user;
+    public String getUID_user() {
+        return this.UID_user;
     }
-    public Double getStars(){
+
+    public Double getStars() {
         return this.stars;
     }
-    public String getReview(){
+
+    public String getReview() {
         return this.review;
     }
 
 
-    public void setUID_user(String uid){
-       this.UID_user = uid;
+    public void setUID_user(String uid) {
+        this.UID_user = uid;
     }
-    public void setStars(Double x){
-        this.stars= x;
+
+    public void setStars(Double x) {
+        this.stars = x;
     }
-    public void setReview(String s){
-        this.review=s;
+
+    public void setReview(String s) {
+        this.review = s;
     }
 }
